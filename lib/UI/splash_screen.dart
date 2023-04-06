0971f44ae0565dd82052160a969e7a5f09435b02
Preetam Chhimpa <preetamchhimpa2022@gmail.com>
2023-04-06 19:52:16 +0530

    splash screen UI added

---
+++ lib/UI/splash_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import '../firebase_Services/splash_servicesdart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  SplashServices SplashScreen = SplashServices();
+
+  @override
+  void initState(){
+    super.initState();
+    SplashScreen.isLogin(context);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Voice Assistant', style: TextStyle(fontSize: 40),
+        ),
+        ),
+    );
+  }
+}
