622a402965e619ec5273adc68b8a60c760e7bdae
Preetam Chhimpa <preetamchhimpa2022@gmail.com>
2023-04-07 02:52:32 +0530

    old file kept

---
+++ lib/extras/api_services.dart
@@ -0,0 +1,41 @@
+// import 'dart:convert';
+
+// import 'package:http/http.dart' as http;
+
+// String apikey = "";
+
+// class ApiServcies{ 
+//   static String baseUrl = "https://api.openai.com/v1/completions";
+
+//  static Map<String,String> header = {
+//   'content-Type': 'application/json',
+//   'Authorization': 'Bearer $apikey'};
+
+
+// static sendmessage(String? message)async{
+//   var response = await http.post(
+//     Uri.parse(baseUrl),
+//     headers: header,
+//     body: jsonEncode({
+//       "model": "text-davinci-003",
+//       "prompt": '$message',
+//       "temperature": 0,
+//       "max_tokens": 100,
+//       "top_p":1,
+//       "frequency_penalty": 0.0,
+//       "presence_penalty": 0.0,
+//       "stop": [" Human:", " AI:"]
+//     }),
+//   );
+
+//   if(response.statusCode == 200){
+//     var data = jsonDecode(response.body.toString());
+//     var msg = data['choices'][0]['text'];
+//     return msg;
+//   }
+//   else{
+//     print("Failed to fetch data");
+//   }
+// }
+// }
+
